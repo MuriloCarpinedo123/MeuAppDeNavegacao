6f5962b29b6c8970bcdb21641fd15f7761f3d287
MuriloCarpinedo123 <carpinedo.muri@gmail.com>
2025-05-22 17:48:43 -0300

    atualização 3.0

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
+import HomeScreen from 'HomeScreen'
+import DetailsScreen from 'DetailsScreen';
+import ProfileScreen from 'ProfileScreen';
+import ProfileScreen from 'SignUpScreen';
 
 const Stack = createStackNavigator();
 
